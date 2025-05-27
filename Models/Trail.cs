b6c60530fc8965f0a26645e7509508849dfababd
Shinigaminai <24736346+Shinigaminai@users.noreply.github.com>
2025-05-27 19:15:45 +0200

    fix: default trails image icon

--- Models/Trail.cs
+++ Models/Trail.cs
@@ -2,8 +2,8 @@ namespace Geotracker.Models
 {
     public class Trail
     {
-        public string Name { get; set; }
-        public string Location { get; set; }
-        public string ImageUrl { get; set; }  // e.g. local image or URL
+        public required string Name { get; set; }
+        public string? Location { get; set; }
+        public string ImageUrl { get; set; } = "path.svg";
     }
 }
