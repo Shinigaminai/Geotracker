33437ff23c8edf864916dc1e2e8267672116b600
Shinigaminai <24736346+Shinigaminai@users.noreply.github.com>
2025-06-15 09:02:24 +0200

    feat: moved trails into bottom drawer

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -7,11 +7,53 @@ public partial class MainPage : ContentPage
 {
 	int count = 0;
 
+	private double _startY;
+	private bool _isOpen = false;
+
 	public MainPage()
 	{
 		InitializeComponent();
 		BindingContext = new MainPageViewModel();
 
 		TrailMap.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+	}
+
+	void OnDrawerPanUpdated(object sender, PanUpdatedEventArgs e)
+	{
+		switch (e.StatusType)
+		{
+			case GestureStatus.Started:
+				_startY = BottomDrawer.TranslationY;
+				break;
+
+			case GestureStatus.Running:
+				var newY = _startY + e.TotalY;
+				BottomDrawer.TranslationY = Math.Max(0, Math.Min(250, newY));
+				break;
+
+			case GestureStatus.Completed:
+				// Animate open/close based on where it stopped
+				if (BottomDrawer.TranslationY < 125)
+				{
+					OpenDrawer();
+				}
+				else
+				{
+					CloseDrawer();
+				}
+				break;
+		}
+	}
+
+	private async void OpenDrawer()
+	{
+		_isOpen = true;
+		await BottomDrawer.TranslateTo(0, 0, 250, Easing.CubicOut);
+	}
+
+	private async void CloseDrawer()
+	{
+		_isOpen = false;
+		await BottomDrawer.TranslateTo(0, 250, 250, Easing.CubicIn);
 	}
 }
